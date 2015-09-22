366ff8cabef466e44947ba0c4d8948e8afd6bec5
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2015-09-22 17:43:41 +0100

    test function added to the code

--- Hello/hello.cpp
+++ Hello/hello.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+
+// This function is never called in the main. What happens to the coverage? 
+void print_hello()
+{
+    std::cout<<"This function prints hello"<<std::endl;
+}
+
 int main(void)
 {
     std::cout<<"Hello"<<std::endl;
